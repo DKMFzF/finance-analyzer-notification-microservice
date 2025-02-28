17f6c2ef26b7e774f1f14dc6bc3c27d58e184d16
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-28 10:01:31 +0700

    update notification-service

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const { Kafka } = require('kafkajs');
+const nodemailer = require('nodemailer');
+
+require('dotenv').config();
 
 // Настройка Kafka
 const kafka = new Kafka({
@@ -7,6 +10,30 @@ const kafka = new Kafka({
 });
 
 const consumer = kafka.consumer({ groupId: 'notification-group' });
+
+// Настройка транспорта для отправки email (используем Gmail)
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+// Функция отправки письма
+async function sendWelcomeEmail(toEmail) {
+  try {
+    await transporter.sendMail({
+      from: `"AI Financial Analyzer" <${process.env.EMAIL_USER}>`,
+      to: toEmail,
+      subject: 'Добро пожаловать!',
+      text: `Привет! Спасибо что используешь наше сервис для анализа финансов!`,
+    });
+    console.log(`[LOG]: Email sent to ${toEmail}`);
+  } catch (error) {
+    console.error('[ERROR]: Failed to send email:', error);
+  }
+}
 
 async function startNotificationService() {
   try {
@@ -18,8 +45,11 @@ async function startNotificationService() {
     await consumer.run({
       eachMessage: async ({ topic, partition, message }) => {
         const user = JSON.parse(message.value.toString());
-        console.log(`[LOG]: Sending welcome email to ${user.login}`);
-        // Здесь можно добавить логику отправки email
+        console.log(`[LOG]: Sending welcome email to ${user.email}`);
+
+        console.log(user)
+
+        await sendWelcomeEmail(user.login);
       },
     });
   } catch (error) {
